9bdb291424be06bd64a4af4558cb01b6e78a4113
0xSilene <0xsilene@MacBook.local>
2019-10-29 17:14:44 -0400

    Fixes

--- scripts/main.js
+++ scripts/main.js
@@ -9,13 +9,12 @@ function onResize() {
 }
 
 function ifmobile() {
-     x = window.isMobile = /iphone|ipod|ipad|android|blackberry|opera mini|opera mobi|skyfire|maemo|windows phone|palm|iemobile|symbian|symbianos|fennec/i.test(navigator.userAgent.toLowerCase());
+    let x = window.isMobile = /iphone|ipod|ipad|android|blackberry|opera mini|opera mobi|skyfire|maemo|windows phone|palm|iemobile|symbian|symbianos|fennec/i.test(navigator.userAgent.toLowerCase());
     if x {
         var list = document.getElementById("hide_on_mobile");
         while (list.hasChildNodes()) {  
             list.removeChild(list.firstChild);
         }
-        document.getElementById("hide_on_resize").style.display = "none";
         document.getElementById("is_mobile").style.display = "inline";
     }
 }
