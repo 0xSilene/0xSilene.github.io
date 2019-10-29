e7a1abe1e5f4481c8b5a915263bc92c881f8daa6
0xSilene <0xsilene@MacBook.local>
2019-10-29 17:09:15 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,23 @@
 "use strict";
+
 function onResize() {
     if (screen.availWidth - window.innerWidth === 0) {
         document.getElementById("hide_on_resize").style.display = "inline";
-    }
-    else {
+    } else {
         document.getElementById("hide_on_resize").style.display = "none";
     }
 }
 
 "use strict";
-function ifmobile(){
+
+function ifmobile() {
     let x = window.isMobile = /iphone|ipod|ipad|android|blackberry|opera mini|opera mobi|skyfire|maemo|windows phone|palm|iemobile|symbian|symbianos|fennec/i.test(navigator.userAgent.toLowerCase());
     if x {
-        document.getElementById("hide_on_mobile").style.display = "none";
+        var list = document.getElementById("myList");
+        while (list.hasChildNodes()) {  
+            list.removeChild(list.firstChild);
+        }
         document.getElementById("hide_on_resize").style.display = "none";
         document.getElementById("is_mobile").style.display = "inline";
     }
 }
-
