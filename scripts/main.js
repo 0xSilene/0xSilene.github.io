754f9a5cc119dbd9788f3050884fa546c49ead2d
0xSilene <0xsilene@MacBook.local>
2019-10-29 16:59:11 -0400

    Updated For Mobile

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,3 @@
-// Security
 "use strict";
 function onResize() {
     if (screen.availWidth - window.innerWidth === 0) {
@@ -8,3 +7,14 @@ function onResize() {
         document.getElementById("hide_on_resize").style.display = "none";
     }
 }
+
+"use strict";
+function ifmobile(){
+    let x = window.isMobile = /iphone|ipod|ipad|android|blackberry|opera mini|opera mobi|skyfire|maemo|windows phone|palm|iemobile|symbian|symbianos|fennec/i.test(navigator.userAgent.toLowerCase());
+    if x {
+        document.getElementById("hide_on_mobile").style.display = "none";
+        document.getElementById("hide_on_resize").style.display = "none";
+        document.getElementById("is_mobile").style.display = "inline";
+    }
+}
+
